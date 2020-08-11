4d12fd6c9ca64763ac6b5b8c7b75934ab692a6cc
Soumyabrata Sen <soumyabratasen@hotmail.com>
2020-08-12 03:40:53 +0530

    Initial Commit

---
+++ core-domain/core-domain/src/main/java/com/sam09/saga/coredomain/CoreDomainApplication.java
@@ -0,0 +1,13 @@
+package com.sam09.saga.coredomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreDomainApplication.class, args);
+	}
+
+}
